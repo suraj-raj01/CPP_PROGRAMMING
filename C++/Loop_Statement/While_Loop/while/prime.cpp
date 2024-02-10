02baf8c21fa9a2da5000b27b4c1a192c16762e81
SURAJ-KUMAR <surajk38656@gmail.com>
2024-02-10 13:21:32 +0530

    While Loop

--- C++/Loop_Statement/While_Loop/while/prime.cpp
+++ C++/Loop_Statement/While_Loop/while/prime.cpp
@@ -1,22 +1,27 @@
 // WAP to find out the given number is prime or not.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,k,count=0;
-    cout<<"Enter the number: ";
-    cin>>n;
-    k=n;
-    while(k>=1){
-    if(n%k==0){
-        count++;
-    }
-    k--;
-    }
- if(count==2){
-    cout<<"prime";
- }
- else{
-    cout<<"not prime";
- }
+int main()
+{
+   int n, k, count = 0;
+   cout << "Enter the number: ";
+   cin >> n;
+   k = n;
+   while (k >= 1)
+   {
+      if (n % k == 0)
+      {
+         count++;
+      }
+      k--;
+   }
+   if (count == 2)
+   {
+      cout << "prime";
+   }
+   else
+   {
+      cout << "not prime";
+   }
 }
