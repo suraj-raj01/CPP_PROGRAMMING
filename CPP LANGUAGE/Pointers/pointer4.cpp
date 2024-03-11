3aaa7a7c181556013d19193ac1c0368c03ebddb4
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-11 22:05:50 +0530

    Modified ~ SURAJ

---
+++ CPP LANGUAGE/Pointers/pointer4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {10,5,2,6};
+    int *p = arr;
+    cout<<p<<endl;
+    cout<<&arr[0]<<endl;
+    cout<<arr<<endl;
+    cout<<p<<endl;
+    cout<<arr[0]<<endl;
+    cout<<0[arr];
+}
