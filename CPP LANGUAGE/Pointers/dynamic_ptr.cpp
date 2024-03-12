e59649b1c03291436eb095902e437f35c38fee5c
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-12 19:16:15 +0530

    Modified ~ SURAJ KUMAR

---
+++ CPP LANGUAGE/Pointers/dynamic_ptr.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,c;
+    cout<<"Enter the number of row: ";
+    cin>>r;
+    int **arr = new int*[r];
+    //create
+    for(int i=0; i<r; ++i)
+    {
+        arr[i] = new int[r];
+    }
+    //input
+    for(int i=0; i<r; ++i)
+    {
+        for(int j=0; j<r; ++j)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    // output
+    cout<<"output is: "<<endl;
+        for(int i=0; i<r; ++i)
+    {
+        for(int j=0; j<r; ++j)
+        {
+            cout<<arr[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+}
---
+++ CPP LANGUAGE/Pointers/dynamic_ptr1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,c;
+    cout<<"Enter the number of row: ";
+    cin>>r;
+    cout<<"Enter the number of col: ";
+    cin>>c;
+    int **arr = new int*[r];
+    //create
+    for(int i=0; i<r; ++i)
+    {
+        arr[i] = new int[c];
+    }
+    //input
+    for(int i=0; i<r; ++i)
+    {
+        for(int j=0; j<c; ++j)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    // output
+    cout<<"output is: "<<endl;
+        for(int i=0; i<r; ++i)
+    {
+        for(int j=0; j<c; ++j)
+        {
+            cout<<arr[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+}
