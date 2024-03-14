6c17550f06d2c2a0b7408ce10d8842db3ff3c5eb
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-14 19:29:26 +0530

    Modified ~ SURAJ KUMAR

--- CPP LANGUAGE/Functions/Pre-Define/String/str2.cpp
+++ CPP LANGUAGE/Functions/Pre-Define/String/str2.cpp
@@ -6,5 +6,15 @@ int main()
     char c[20] = "surajkumarrajak";
     int size = sizeof(c)/sizeof(c[0]);
     cout<<size<<endl;
-    cout<<strlen(c);
+    cout<<strlen(c)<<endl;  // using inbuilt function
+    // Without using inbuilt function
+    char d[20];
+    cout<<"Enter your name: ";
+    cin>>d;
+    int s = 0;
+    for(int i=0; d[i] != '\0'; ++i)
+    {
+        s++;
+    }
+    cout<<"Length: "<<s;
 }
---
+++ CPP LANGUAGE/Functions/Pre-Define/String/strFun.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a[20]= "bhopal";
+    char b[20]= "cybrom";
+    char c[20] = "hello";
+    char pwd1[20];
+    char pwd2[20];
+    cout<<c;
+    cout<<"enter name: ";
+    cin>>c;
+}
