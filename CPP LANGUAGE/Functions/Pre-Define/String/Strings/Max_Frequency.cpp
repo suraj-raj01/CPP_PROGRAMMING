c47b2f5c1ecd76603f992188c9ef6e3e0cdde825
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-18 19:28:51 +0530

    Modified By SURAJ KUMAR

--- CPP LANGUAGE/Functions/Pre-Define/String/Strings/Max_Frequency.cpp
+++ CPP LANGUAGE/Functions/Pre-Define/String/Strings/Max_Frequency.cpp
@@ -5,16 +5,28 @@ using namespace std;
 #include<cstring>
 int main()
 {
-   char ch[20] , count;
+   char ch[20];
    cout<<"Enter your name: ";
    cin>>ch;
+   int p = 0;
+   char t;
    int len = strlen(ch);
-   for(int i=0; i<len; i++)
+   for(int i=0; i<len; ++i)
    {
-      if(ch[i]==ch[i+1])
+      int c = 0;
+      for(int j=0; j<len; ++j)
       {
-        count = ch[i];
+         if(ch[i]==ch[j])
+         {
+            c++;
+         }
+      }
+      if(c>p)
+      {
+         p=c;
+         t = ch[i];
       }
    }
-   cout<<"Maximum Frequency Character: "<<count;
+   cout<<"Maximum Frequency Character: "<<t<<"\n";
+   cout<<"Letter come "<<p<<"Times";
 }
--- CPP LANGUAGE/Functions/Pre-Define/String/Strings/ascending_Str.cpp
+++ CPP LANGUAGE/Functions/Pre-Define/String/Strings/ascending_Str.cpp
@@ -2,22 +2,25 @@
 
 #include<iostream>
 using namespace std;
+#include<cmath>
 #include<cstring>
 int main()
 {
-    char ch[20],count;
+    char ch[20];
+    char count;
     cout<<"Enter your String: ";
     cin>>ch;
     int len = strlen(ch);
     for(int i=0; i<len; i++)
     {
-        for(int j=0; j<len-1; ++j)
+        for(int j=0; j<len-i-1; ++j)
         {
-            if(ch[j]>ch[j+1])
+            if(ch[j]>ch[j+1])  // for descending -> if(ch[j] < ch[j+1])
             {
-                count = ch[j+1];
-                ch[j+1] = ch[j];
-                ch[j] = count;
+                // count = ch[j+1];
+                // ch[j+1] = ch[j];
+                // ch[j] = count;
+                swap(ch[j],ch[j+1]);
             }
         }
     }
