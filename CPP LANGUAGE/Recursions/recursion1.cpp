0ed68a553517752c901c088479103487bce1c856
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-19 19:36:57 +0530

    Modified ~ SURAJ KUMAR

--- CPP LANGUAGE/Recursions/recursion1.cpp
+++ CPP LANGUAGE/Recursions/recursion1.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void rec(int n)
 {
-    if(n==0)
+    if (n == 0)
     {
         return;
     }
     // Head Recursion
-    //cout<<n<<"  ";
-    rec(n-1);
+    // cout<<n<<"  ";
+    rec(n - 1);
     // Tail Recursion
-    cout<<n<<" ";
+    cout << n << " ";
 }
 
 int main()
 {
     int n;
-    cout<<"Enter the number: ";
-    cin>>n;
+    cout << "Enter the number: ";
+    cin >> n;
     rec(n);
 }
