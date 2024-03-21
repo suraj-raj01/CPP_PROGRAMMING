916462be3aa2c3f2e360d291df0e22adb959a67f
SURAJ_KUMAR <surajk38656@gmail.com>
2024-03-21 12:53:27 +0530

    Modified By SURAJ KUMAR

--- CPP LANGUAGE/Functions/Pre-Define/String/Strings/Max_Frequency.cpp
+++ CPP LANGUAGE/Functions/Pre-Define/String/Strings/Max_Frequency.cpp
@@ -1,19 +1,49 @@
 // Write a string to count the maximum frequency of any character.
 
+// #include <iostream>
+// using namespace std;
+// #include <cstring>
+// int main()
+// {
+//    char ch[20], t;
+//    cout << "Enter your name: ";
+//    cin >> ch;
+//    int p = 0;
+//    int len = strlen(ch);
+//    for (int i = 0; i < len; ++i)
+//    {
+//       int c = 0;
+//       for (int j = 0; j < len; ++j)
+//       {
+//          if (ch[i] == ch[j])
+//          {
+//             c++;
+//          }
+//       }
+//       if (c > p)
+//       {
+//          p = c;
+//          t = ch[i];
+//       }
+//    }
+//    cout << "Maximum Frequency Character: " << t << "\n";
+//    cout << "Letter came " << p << " Times";
+// }
+
 #include <iostream>
 using namespace std;
 #include <cstring>
 int main()
 {
    char ch[20], t;
-   cout << "Enter your name: ";
+   cout << "Enter a string: ";
    cin >> ch;
    int p = 0;
    int len = strlen(ch);
    for (int i = 0; i < len; ++i)
    {
       int c = 0;
-      for (int j = 0; j < len; ++j)
+      for (int j = 0; j < len; j++)
       {
          if (ch[i] == ch[j])
          {
@@ -26,6 +56,5 @@ int main()
          t = ch[i];
       }
    }
-   cout << "Maximum Frequency Character: " << t << "\n";
-   cout << "Letter came " << p << " Times";
+   cout<<t;
 }
