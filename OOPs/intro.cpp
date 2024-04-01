13c8fb3b2c18a3e1af726472b8ed97893ef0a69d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-01 19:31:52 +0530

    Modified By SURAJ KUMAR

---
+++ OOPs/intro.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+// class show{
+//   int a;
+//   double d;
+//   char c;
+// };
+
+class show{
+
+    // data member --> variable
+    // member function --> functions.
+    int a,b;
+    public:void sum()
+    {
+        a=10,b=20;
+        cout<<a+b;
+    }
+    void hello()
+    {
+        cout<<"Hello!!";
+    }
+};
+
+int main()
+{
+//   show obj;
+//   cout<<sizeof(obj);
+    show obj;
+    obj.sum();
+    obj.hello();
+}
