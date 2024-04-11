8c05c8e2e975378fd3ebd1ecd3ba4c9ff5dc4cc9
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-11 19:37:09 +0530

    Shallow Copy

---
+++ OOPs/Constructors/CopyConstructor/ShallowCopy1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Main{
+    int a;
+    int *p;
+    public:Main()
+    {
+       a=0;
+       p=new int;
+       p=0;
+    }
+    Main(int x, int y)
+    {
+       a = x;
+       p = &y;
+    }
+    void update()
+    {
+        a=a+1;
+        *p=*p+1;
+    }
+    void show()
+    {
+        cout<<"a : "<<a<<endl;
+        cout<<"b : "<<*p<<endl;
+    }
+};
+
+int main()
+{
+  Main obj(20,10);
+  obj.show();
+  Main obj1(obj);
+  obj1.update();
+  obj1.show();
+  obj.show();
+}
