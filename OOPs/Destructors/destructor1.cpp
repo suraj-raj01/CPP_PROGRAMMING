0156bd1456e349cd3d8b05db169e9902be8a78cc
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-29 02:14:40 +0530

    Modified By SURAJKUMAR

--- OOPs/Destructors/destructor1.cpp
+++ OOPs/Destructors/destructor1.cpp
@@ -1,35 +1,36 @@
 /*
 Destructor:-
 Destructor is used to deallocate the memory of an object it calls implicitly.
-the destructor is define by using tild(~) symbol along with class name and 
+the destructor is define by using tild(~) symbol along with class name and
 without using any parameter.
 
-Destructor is an instance member function that is invoked automatically whenever an 
-object is going to be destroyed. Meaning, a destructor is the last function that is 
+Destructor is an instance member function that is invoked automatically whenever an
+object is going to be destroyed. Meaning, a destructor is the last function that is
 going to be called before an object is destroyed.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Main{
-    public: void show()
+class Main
+{
+public:
+    void show()
     {
-        cout<<"Example of Destructor!!\n";
+        cout << "Example of Destructor!!\n";
     }
-    ~Main()  // creating destructor
+    ~Main() // creating destructor
     {
-        cout<<"Memory released!!\n";
+        cout << "Memory released!!\n";
     }
     Main()
     {
-        cout<<"Memory allocated!!\n";
+        cout << "Memory allocated!!\n";
     }
-
 };
 
 int main()
 {
-  Main obj;
-  obj.show();  
+    Main obj;
+    obj.show();
 }
--- OOPs/OperatorOverloading/binaryOperator1.cpp
+++ OOPs/OperatorOverloading/binaryOperator1.cpp
@@ -1,32 +1,67 @@
+// #include<iostream>
+// using namespace std;
+
+// class Main{
+//     int x,y;
+//     public:Main(int x=0,int y=0):x{x},y{y} // initializer List
+//     {
+//         // this->x = x;
+//         // this->y = y;
+//     }
+//     Main operator +(Main &obj)
+//     {
+//         Main g1;
+//         g1.x = x+obj.x;
+//         g1.y = y+obj.y;
+//         return g1;
+//     }
+//     void show()
+//     {
+//         cout<<"x : "<<x<<endl;
+//         cout<<"y : "<<y<<endl;
+//     }
+// };
+
+// int main()
+// {
+//    Main g1(10,20);
+//    Main g2(15,25);
+//    Main g3;
+//    g3 = g1+g2;
+//    g3.show();
+// }
+
+// <-------------------OPERATOR MainING-------------------->
+
 #include<iostream>
 using namespace std;
 
-class Main{
-    int x,y;
-    public:Main(int x=0,int y=0):x{x},y{y} // initializer List
-    {
-        // this->x = x;
-        // this->y = y;
-    }
-    Main operator +(Main &obj)
-    {
-        Main g1;
-        g1.x = x+obj.x;
-        g1.y = y+obj.y;
-        return g1;
-    }
-    void show()
-    {
-        cout<<"x : "<<x<<endl;
-        cout<<"y : "<<y<<endl;
-    }
+class Main
+{
+int x;
+int y;
+public:
+Main(int x=0, int y=0) : x{x},y{y}
+{}
+Main operator +(Main &obj)
+{
+Main g;
+g.x = x + obj.x;
+g.y = y + obj.y;
+return g;
+}
+void show()
+{
+cout<<"x: "<<x<<endl;
+cout<<"y: "<<y<<endl;
+}
 };
 
 int main()
 {
-   Main g1(10,20);
-   Main g2(15,25);
-   Main g3;
-   g3 = g1+g2;
-   g3.show();
+Main obj1(10,20);
+Main obj2(15,10);
+Main obj3;
+obj3 = obj1+obj2;
+obj3.show();
 }
