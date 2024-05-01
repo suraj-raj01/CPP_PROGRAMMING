226ce1ab70ca66a1a2309aa41eaadc8f2988297d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-01 15:28:35 +0530

    Modified By SURAJ KUMAR

--- SHOP/Shop.cpp
+++ SHOP/Shop.cpp
@@ -4,12 +4,12 @@ Data Members:
 ItemId
 ItemName
 ItemPrice
+RemoveData
 Total
 count
 
 Member Functions
 add()
-calculat()
 show()
 delet()
 */
@@ -24,13 +24,14 @@ class SHOPING
     int ItemPrice[100];
     int total = 0;
     int count = 0;
+    int remove = 0;
 
     public:
     void add()
     {
         for(int i=0; i<100; i++)
         {
-            cout<<"Enter the ItemId: ";
+            cout<<"\nEnter the ItemId: ";
             cin>>ItemId[i];
             cout<<"Enter the ItemName: ";
             cin>>ItemName[i];
@@ -38,7 +39,7 @@ class SHOPING
             cin>>ItemPrice[i];
             char ch;
             cout<<"\n----------------------------\n";
-            cout<<"|  Enter A for add Item    |\n|  Enter P for print Bill: |";
+            cout<<"|  Enter A for add Item    |\n|  Enter P for print Bill  |";
             cout<<"\n----------------------------\n";
             cin>>ch;
             count++;
@@ -46,9 +47,10 @@ class SHOPING
             {
                 continue;
             }
-            else{
+            else if(ch=='p' || ch=='P'){
                 break;
             }
+            cout<<"\nPlease enter valid input!!\n\n";
         }
     };
 
@@ -59,38 +61,67 @@ class SHOPING
        cout<<"\n-----------------------------------------\n";
        for(int i=0; i<count; i++)
        {
-          cout<<ItemId[i]<<"\t\t "<<ItemName[i]<<"\t\t "<<ItemPrice[i]<<endl;
+          cout<<"  "<<ItemId[i]<<"\t\t"<<ItemName[i]<<"\t\t  "<<ItemPrice[i]<<endl;
           total+=ItemPrice[i];
        }
        cout<<"-----------------------------------------\n";
        cout<<"\nTotal Item: "<<count;
+       cout<<"\nRemoved Item: "<<remove;
+       cout<<"\nTotal New: "<<count-remove;
        cout<<"\nTotal Bill: "<<total<<endl;
+       CANCEL();
     }
 
     void CANCEL()
     {
         total = 0;
         char cancel;
-        cout<<"\n--------------------------\n";
-        cout<<"Enter Y for Cancel Item\n";
-        cout<<"Enter N for Not Cancel Item";
-        cout<<"\n--------------------------\n";
+        cout<<"\n-------------------------------\n";
+        cout<<"| Enter Y for Cancel Item     |\n";
+        cout<<"| Enter N for Not Cancel Item |";
+        cout<<"\n-------------------------------\n";
         cin>>cancel;
         if(cancel=='y'||cancel=='Y')
         {
         int del;
-        cout<<"Enter the ItemId: ";
+        cout<<"Enter ItemId for Cancel Item: ";
         cin>>del;
         for(int i=0; i<count; i++)
         {
-            if(ItemId[i]==del)
+            if(del==ItemId[i])
+            {
             ItemId[i] = 0;
-            ItemName[i] = "?";
+            ItemName[i] = "-";
             ItemPrice[i] = 0;
-            total = 0;
+            remove++;
+            }
         }
+        show();
+        }
+        else if(cancel=='n' || cancel=='N'){
+           EXIT();
         }
         else{
+            cout<<"\nPlease enter Valid Operator!!\n";
+            CANCEL();
+        }
+    }
+    
+    void EXIT()
+    {
+        char x;
+        cout<<"\n----------------------------------\n";
+        cout<<"Enter X for exit this Windows\nEnter Y for delete Items\n";
+        cout<<"----------------------------------\n";
+        cin>>x;
+        if (x=='y' || x=='Y')
+        {
+            CANCEL();
+        }
+        else
+        {
+        cout<<"\nThankyou For Visiging!!\n\n";
+        exit;
         }
     }
 };
@@ -99,8 +130,6 @@ int main()
 {
    SHOPING s;
    s.add();
-   s.show();
-   s.CANCEL();
    s.show();
 }
 
