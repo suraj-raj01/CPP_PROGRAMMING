76424803a979ff05993475c78ffcd3f1672d3bb2
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-06 00:49:31 +0530

    Modified By SURAJ KUMAR

--- DSA/Sorting/bubbleSort.cpp
+++ DSA/Sorting/bubbleSort.cpp
@@ -1,1 +1,41 @@
 // Bubble Sort...
+
+
+#include<iostream>
+using namespace std;
+#include<cmath>
+
+void BubbleSort(int arr[], int size)
+{
+    int swaped=0;
+    for(int i=0; i<size; i++)
+    {
+        for(int j=0; j<i-1; j++)
+        {
+            if(arr[i]<arr[j+1])
+            {
+                swap(arr[i],arr[j+1]);
+                swaped++;
+            }
+        }
+    }
+    cout<<"Swaped : "<<swaped<<" Times"<<endl;
+}
+void print(int arr[],int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+}
+
+int main()
+{
+int n=5;
+int arr[] = {1,5,3,2,4};
+cout<<"Before Bubble Sort\n";
+print(arr,n);
+cout<<"\nAfter Bubble Sort\n";
+BubbleSort(arr,n);
+print(arr,n);
+}
--- FinalTest/third.cpp
+++ FinalTest/third.cpp
@@ -1,10 +1,10 @@
 /*
-Q.3 Write a program to maintain the records of person with details (Name and Age) and 
-find the eldest among them. The program must use parametrized constructor to initialize 
+Q.3 Write a program to maintain the records of person with details (Name and Age) and
+find the eldest among them. The program must use parametrized constructor to initialize
 the value.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Person
@@ -12,57 +12,58 @@ class Person
     string name[5];
     int age[5];
     int eldest = 0;
-    public:
+
+public:
     void initialize(string s[], int a[])
     {
-        for(int i=0; i<5; i++)
+        for (int i = 0; i < 5; i++)
         {
             name[i] = s[i];
         }
-        for(int i=0; i<5; i++)
+        for (int i = 0; i < 5; i++)
         {
             age[i] = a[i];
         }
     }
     void show()
     {
-        cout<<"NAME \t\tAGE\n";
-        for(int i=0; i<5; i++)
+        cout << "NAME \t\tAGE\n";
+        for (int i = 0; i < 5; i++)
         {
-            cout<<name[i]<<"\t\t"<<age[i]<<endl;
+            cout << name[i] << "\t\t" << age[i] << endl;
         }
     }
     void older()
     {
-        cout<<"\n";
-        cout<<"Eldest: \n";
-        for(int i=0; i<5; i++)
+        cout << "\n";
+        cout << "Eldest: \n";
+        for (int i = 0; i < 5; i++)
         {
-            if(eldest < age[i])
+            if (eldest < age[i])
             {
                 eldest = age[i];
             }
         }
-        cout<<"eldest is: "<<eldest<<endl;
+        cout << "eldest is: " << eldest << endl;
     }
 };
 
 int main()
 {
-string name[5];
-int age[5];
-cout<<"Enter the name: \n";
-for(int i=0; i<5; i++)
-{
-    cin>>name[i];
-}
-cout<<"Enter the age: ";
-for(int i=0; i<5; i++)
-{
-    cin>>age[i];
-}
-Person p;
-p.initialize(name,age);
-p.show();
-p.older();
+    string name[5];
+    int age[5];
+    cout << "Enter the name: \n";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> name[i];
+    }
+    cout << "Enter the age: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> age[i];
+    }
+    Person p;
+    p.initialize(name, age);
+    p.show();
+    p.older();
 }
