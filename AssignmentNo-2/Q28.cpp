423924d78aff7c834aeca96e9221c6ff40cfd092
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-05 14:04:01 +0530

    AssignmentNo-2

--- AssignmentNo-2/Q28.cpp
+++ AssignmentNo-2/Q28.cpp
@@ -2,3 +2,36 @@
 28. Write a program to find all prime factors of a number.
 */
 
+#include<iostream>
+using namespace std;
+
+bool isPrimeFact(int n)
+{
+    for(int i=2; i<=n/2; i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void printPrime(int n)
+{
+    int sum=0;
+    for(int i=2; i<=n; i++)
+    {
+        if(isPrimeFact(i))
+        {
+            cout<<i<<" ";
+        }
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    printPrime(n);
+}
