f334d49ffe7875b2c3a9ae9b192f34536e8b9631
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-14 15:01:46 +0530

    DSA/Vector/task.cpp

---
+++ DSA/Vector/task.cpp
@@ -0,0 +1,32 @@
+/*
+WAP to sum of two values by using target values.
+Target value: 6
+vector<int>{2,1,4,9,3}
+index: 0,2
+value: 2,4
+*/
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+int main()
+{
+    vector<int>a = {2,1,4,9,3};
+    int target;
+    cout<<"Enter the target number: ";
+    cin>>target;
+    for(int i=0; i<a.size()-1; i++)
+    {
+        for(int j=i+1; j<a.size(); i++)
+        {
+            if(a[i]+a[j]==target)
+            {
+                cout<<"Values: ";
+                cout<<a[i]<<"  "<<a[j];
+                cout<<"\nIndex: ";
+                cout<<j<<" "<<i;
+            }
+        }
+    }
+}
