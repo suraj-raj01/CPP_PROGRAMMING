568da8fff6db3da5f0dd966382849e0fa3904c22
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-17 02:01:22 +0530

    Task

--- DSA/Task/first.cpp
+++ DSA/Task/first.cpp
@@ -1,5 +1,6 @@
 /*
-Q.1 Given an array of size n-1 such that it only contains distinct integers in the range of 1 to n. Return the missing element.
+Q.1 Given an array of size n-1 such that it only contains distinct integers in the 
+range of 1 to n. Return the missing element.
 Examples:
 Input: n = 5, arr[] = {1,2,3,5} Output: 4
 Input: n = 2, arr[] = {1} Output: 2
