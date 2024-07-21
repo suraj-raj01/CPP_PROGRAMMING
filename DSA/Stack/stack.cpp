6a8540373a02ac64bcbeec9dad6696b5a5f072a1
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-21 20:40:26 +0530

    Stack Practice

--- DSA/Stack/stack.cpp
+++ DSA/Stack/stack.cpp
@@ -1,59 +1,136 @@
+// #include<iostream>
+// using namespace std;
+// #define n 5
+
+// class stk{
+//     public:int *arr;
+//     int top;
+//     stk(){
+//         arr = new int[n];
+//         top=-1;
+//     }
+//     void push(int a)
+//     {
+//         if(top==n-1)
+//         {
+//             cout<<"Overflow\n";
+//             return;
+//         }
+//         top++;
+//         arr[top] = a;
+//     }
+//     void pop()
+//     {
+//         if(top==-1)
+//         {
+//             cout<<"No element to delete\n";
+//             return;
+//         }
+//         top--;
+//     }
+//     int tops()
+//     {
+//         if(top==-1)
+//         {
+//             cout<<"No element\n";
+//             return -1;
+//         }
+//         return arr[top];
+//     }
+// };
+
+// int main()
+// {
+//     stk st;
+//     st.push(10);
+//     st.push(20);
+//     st.push(30);
+//     st.push(40);
+//     st.push(50);
+//     cout<<st.tops()<<"\n";
+//     st.pop();
+//     cout<<"top element is = "<<st.tops()<<endl;
+//     while(st.top!=-1)
+//     {
+//         cout<<st.tops()<<endl;
+//         st.pop();
+//     }
+//     delete []st.arr;
+// }
+
+
 #include<iostream>
 using namespace std;
 #define n 5
-
-class stk{
-    public:int *arr;
+class stack{
+    public:
+    int *stk;
     int top;
-    stk(){
-        arr = new int[n];
+    stack()
+    {
+        stk = new int[n];
         top=-1;
     }
-    void push(int a)
+    void push(int data)
     {
         if(top==n-1)
         {
-            cout<<"Overflow\n";
+            cout<<"Overflow Condition!!!";
             return;
         }
-        top++;
-        arr[top] = a;
+        else{
+            top++;
+            stk[top] = data;
+        }
     }
-    void pop()
+    int pop()
     {
+        int r=-1;
         if(top==-1)
         {
-            cout<<"No element to delete\n";
-            return;
+            cout<<"\nUnderflow Condition!!!";
         }
-        top--;
+        else{
+            r = stk[top];
+            top--;
+        }
+        return r;
     }
-    int tops()
+    int peek()
     {
+        int r = -1;
         if(top==-1)
         {
-            cout<<"No element\n";
-            return -1;
+            cout<<"\nEmpty stack";
         }
-        return arr[top];
+        else{
+            r = stk[top];
+        }
+        return r;
+    }
+    void show()
+    {
+        for(int i=n-1; i>=0; i--)
+        {
+            cout<<"\n"<<stk[i]<<"  ";
+        }
     }
 };
-
 int main()
 {
-    stk st;
-    st.push(10);
-    st.push(20);
-    st.push(30);
-    st.push(40);
-    st.push(50);
-    cout<<st.tops()<<"\n";
-    st.pop();
-    cout<<"top element is = "<<st.tops()<<endl;
-    while(st.top!=-1)
-    {
-        cout<<st.tops()<<endl;
-        st.pop();
-    }
-    delete []st.arr;
+    stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    s.show();
+    cout<<"\nPeek element is: "<<s.peek();
+    cout<<"\nDeleted Element is: "<<s.pop();
+    cout<<"\nDeleted Element is: "<<s.pop();
+    cout<<"\nDeleted Element is: "<<s.pop();
+    cout<<"\nPeek element is: "<<s.peek();
+    cout<<"\nDeleted Element is: "<<s.pop();
+    cout<<"\nDeleted Element is: "<<s.pop();
+    cout<<"\nDeleted Element is: "<<s.pop();
 }
