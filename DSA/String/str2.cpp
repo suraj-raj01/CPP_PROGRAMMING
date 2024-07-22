5ac1a71bdb3f1ab291e82a1b5c25e54e3f098cbd
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-22 13:13:02 +0530

    Revise String

--- DSA/String/str2.cpp
+++ DSA/String/str2.cpp
@@ -14,4 +14,27 @@ int main()
 
     sort(s1.begin(),s1.end(),greater<int>());   //decending
     cout<<s1<<endl;
+
+    // anagram
+    string a = "abcd";
+    string b = "dcba";
+    int count=0;
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    int l=0;
+    for(int i=0; i<a.length(); i++)
+    {
+        if(a[l]==b[i])
+        {
+            count++;
+            l++;
+        }
+    }
+    if(count==a.length())
+    {
+        cout<<"Anagram";
+    }
+    else{
+        cout<<"Not Anagram";
+    }
 }
