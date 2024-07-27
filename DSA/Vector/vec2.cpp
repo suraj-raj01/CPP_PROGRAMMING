84070ec73abd5d857e4c7d93ef9ca72fcd897d89
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-27 14:41:28 +0530

    Vector

--- DSA/Vector/vec2.cpp
+++ DSA/Vector/vec2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     vector<int>a = {8,4,61,5,4,2,1,3,5,4,2,4};
-    cout<<count(a.begin(),a.end(),4);
+    cout<<count(a.begin(),a.end(),2);
     cout<<"\nMin element index:";
     cout<<min_element(a.begin(),a.end())-a.begin();
     cout<<"\nMax element index:";
@@ -13,6 +13,5 @@ int main()
     cout<<"\n";
     cout<<"Min element: "<<*min_element(a.begin(),a.end())<<"\n";
     cout<<"Max element: "<<*max_element(a.begin(),a.end())<<"\n";
-
     
 }
