dd135aec03472702b68a7b6c4e5a8e31e799a7bf
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-15 12:42:47 +0530

    SingleNumber (leetcode problem)

--- STL/Vector/SingleElement.cpp
+++ STL/Vector/SingleElement.cpp
@@ -6,12 +6,10 @@ You must implement a solution with a linear runtime complexity and use only cons
 extra space.
 
 Example 1:
-
 Input: nums = [2,2,1]
 Output: 1
 
 Example 2:
-
 Input: nums = [4,1,2,1,2]
 Output: 4
 
